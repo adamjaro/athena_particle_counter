6fa292ba16cc1148ff1c41cb13f094218924d7d4
Jaroslav Adam <jaroslav.adam@cern.ch>
2021-11-23 15:10:59 -0500

    Hit rates

--- macro/hit_rate/plot_hits.py
+++ macro/hit_rate/plot_hits.py
@@ -30,44 +30,45 @@ def hit_en():
 
     #hit energy
 
-    infile = "../ddhits/ddhits.root"
+    #infile = "../ddhits/ddhits.root"
+    #infile = "/home/jaroslav/sim/Athena/data/beam-gas/cnt1a/ddhits_f1000.root"
+    infile = "/home/jaroslav/sim/Athena/data/pythia6/py10x100a/ddhits_f10.root"
 
-    #log_10(keV)
-    emin = -9
-    emax = 9
-    ebin = 0.1
-
-    #det = "VertexBarrelHits"
-    #det = "TrackerBarrelHits"
-    #det = "TrackerEndcapHits"
-    det = "EcalEndcapNHits"
+    #det = {"VertexBarrelHits": "blue", "TrackerBarrelHits": "gold", "TrackerEndcapHits": "red"}
+    #det = {"EcalEndcapPHits": "blue", "EcalEndcapNHits": "gold", "EcalBarrelHits": "lime", "EcalBarrelScFiHits": "red"}
+    det = {"HcalBarrelHits": "blue", "HcalEndcapNHits": "gold", "HcalEndcapPHits": "red"}
 
     inp = TFile.Open(infile)
-    htree = inp.Get(det)
 
-    hE = ut.prepare_TH1D("hE", ebin, emin, emax)
-    htree.Draw("TMath::Log10(en*1e6) >> hE") # keV
-    ep = ut.h1_to_arrays(hE)
+    ep = {}
+    for i in det:
+        ep[i] = get_hit_en(inp, i)
 
     #plot
-    plt.style.use("dark_background")
-    col = "lime"
-    #col = "black"
+    #plt.style.use("dark_background")
+    #col = "lime"
+    col = "black"
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     set_axes_color(ax, col)
     set_grid(plt, col)
 
-    plt.plot(ep[0], ep[1], "-", color="blue", lw=1)
+    leg = legend()
+    for i in ep:
 
-    ax.set_xlabel("keV")
-    ax.set_ylabel("Counts")
+        plt.plot(ep[i][0], ep[i][1], "-", color=det[i], lw=1)
+        leg.add_entry(leg_lin(det[i]), i[0:-4])
+
+    ax.set_xlabel("Hit energy (keV)")
+    ax.set_ylabel("Normalized counts")
 
     ax.set_yscale("log")
 
-    #plt.xticks(ax.get_xticks()[1:-1], ["$10^{"+"{0:.0f}".format(i)+"}$" for i in ax.get_xticks()[1:-1]])
-    plt.xticks(ax.get_xticks()[1:-1], ["$10^{"+"{0:.1f}".format(i)+"}$" for i in ax.get_xticks()[1:-1]])
+    plt.xticks(ax.get_xticks()[1:-1], ["$10^{"+"{0:.0f}".format(i)+"}$" for i in ax.get_xticks()[1:-1]])
+    #plt.xticks(ax.get_xticks()[1:-1], ["$10^{"+"{0:.1f}".format(i)+"}$" for i in ax.get_xticks()[1:-1]])
+
+    leg.draw(plt, col)
 
     fig.savefig("01fig.pdf", bbox_inches = "tight")
     plt.close()
@@ -75,14 +76,33 @@ def hit_en():
 #hit_en
 
 #_____________________________________________________________________________
+def get_hit_en(inp, det):
+
+    #log_10(keV)
+    emin = -9
+    emax = 9
+    ebin = 0.1
+
+    htree = inp.Get(det)
+
+    hE = ut.prepare_TH1D("hE", ebin, emin, emax)
+    htree.Draw("TMath::Log10(en*1e6) >> hE") # keV
+    ut.norm_to_integral(hE, 1.)
+
+    return ut.h1_to_arrays(hE)
+
+#get_hit_en
+
+#_____________________________________________________________________________
 def hit_rate():
 
     #hit rate
 
-    infile = "../ddhits/ddhits.root"
+    #infile = "../ddhits/ddhits.root"
+    infile = "/home/jaroslav/sim/Athena/data/beam-gas/cnt1a/ddhits_f1000.root"
 
     #number of simulated events
-    nsim = 2000.
+    nsim = 1e6
 
     det = [ \
 	"VertexBarrelHits", \
@@ -104,13 +124,17 @@ def hit_rate():
 
     bg = get_rate_beam_gas(infile, nsim, det)
 
-    get_rate_pythia6()
+    in_py = "/home/jaroslav/sim/Athena/data/pythia6/py10x100a/ddhits_f10.root"
+    nsim_py = 44000.
 
-    #print(bg)
+    py = get_rate_pythia6(in_py, nsim_py, det)
+
+    #print(py)
 
     idet = 0
     xp = []
     yp = []
+    ypy = []
     labels = ([], [])
     for i in bg:
         xp.append(idet-0.5)
@@ -119,29 +143,41 @@ def hit_rate():
         yp.append(bg[i])
         yp.append(bg[i])
 
+        ypy.append(py[i])
+        ypy.append(py[i])
+
         labels[0].append( idet )
         labels[1].append( i[0:-4] )
 
         idet += 1
 
+    #table
+    for i in bg:
+        print(i[0:-4], " & {0:.3f}".format(bg[i]*1e-6), " & {0:.3f}".format(py[i]*1e-6), r"\\") # MHz
+
     #plot
-    plt.style.use("dark_background")
-    col = "lime"
-    #col = "black"
+    #plt.style.use("dark_background")
+    #col = "lime"
+    col = "black"
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     set_axes_color(ax, col)
     set_grid(plt, col)
 
-    plt.plot(xp, yp, "-", color="blue", lw=1)
+    plt.plot(xp, ypy, "-", color="blue", lw=1)
+    plt.plot(xp, yp, "-", color="red", lw=1)
 
-    #ax.set_xlabel("det")
-    ax.set_ylabel("Hits")
+    ax.set_ylabel("Number of hits per second")
 
     ax.set_yscale("log")
 
     plt.xticks(labels[0], labels[1], rotation = "vertical")
+
+    leg = legend()
+    leg.add_entry(leg_lin("red"), "Electron beam-gas, 10 GeV")
+    leg.add_entry(leg_lin("blue"), "Pythia6, 10x100 GeV")
+    leg.draw(plt, col)
 
     fig.savefig("01fig.pdf", bbox_inches = "tight")
     plt.close()
@@ -176,9 +212,7 @@ def get_rate_beam_gas(infile, nsim, det):
 #get_rate_beam_gas
 
 #_____________________________________________________________________________
-def get_rate_pythia6():
-
-    #infile, nsim, det
+def get_rate_pythia6(infile, nsim, det):
 
     #Pythia6 total cross section
     sigma = 40.891e-3 # mb
@@ -189,7 +223,22 @@ def get_rate_pythia6():
     #production rate for Pythia
     prod_rate = sigma*lumi_cmsec*1e-27 # to mb
 
-    print(prod_rate)
+    #rate per simulated event
+    rsim = prod_rate/nsim
+    #print("rsim:", rsim)
+
+    inp = TFile.Open(infile)
+
+    rate = {}
+    for i in det:
+        htree = inp.Get(i)
+
+        rate[i] = htree.GetEntries()*rsim
+
+    #sort by the rate
+    #rate = dict(sorted(rate.items(), key=lambda item: -item[1]))
+
+    return rate
 
 #_____________________________________________________________________________
 def set_axes_color(ax, col):
@@ -210,6 +259,34 @@ def set_grid(px, col="lime"):
     px.grid(True, color = col, linewidth = 0.5, linestyle = "--")
 
 #set_grid
+
+#_____________________________________________________________________________
+class legend:
+    def __init__(self):
+        self.items = []
+        self.data = []
+    def add_entry(self, i, d):
+        self.items.append(i)
+        self.data.append(d)
+    def draw(self, px, col=None, **kw):
+        leg = px.legend(self.items, self.data, **kw)
+        if col != None:
+            px.setp(leg.get_texts(), color=col)
+            if col != "black":
+                leg.get_frame().set_edgecolor("orange")
+        return leg
+
+#_____________________________________________________________________________
+def leg_lin(col, sty="-"):
+    return Line2D([0], [0], lw=2, ls=sty, color=col)
+
+#_____________________________________________________________________________
+def leg_txt():
+    return Line2D([0], [0], lw=0)
+
+#_____________________________________________________________________________
+def leg_dot(fig, col, siz=8):
+    return Line2D([0], [0], marker="o", color=fig.get_facecolor(), markerfacecolor=col, markersize=siz)
 
 #_____________________________________________________________________________
 if __name__ == "__main__":
